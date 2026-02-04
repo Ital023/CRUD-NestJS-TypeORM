7cac0c892568e8c4ce07df9d964527cd53540e5b
Ital023 <contadoitalo911@gmail.com>
2026-02-04 12:12:25 -0300

    feat: removing the interceptors

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -18,10 +18,8 @@ import { CreateRecadoDto } from './dto/create-recado.dto';
 import { UpdateRecadoDto } from './dto/update-recado.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { ParseIntIdPipe } from 'src/common/pipes/parse-int-id.pipe';
-import { AddHeaderInterceptor } from 'src/common/interceptors/add-header.interceptor';
 
 @Controller('recados')
-@UseInterceptors(AddHeaderInterceptor)
 export class RecadosController {
   constructor(private readonly recadosService: RecadosService) {}
 
