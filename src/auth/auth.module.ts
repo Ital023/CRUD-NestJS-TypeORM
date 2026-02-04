c4eca54fcf843b8ab8e4bf49436ac020b643af38
Ital023 <contadoitalo911@gmail.com>
2026-02-04 20:00:15 -0300

    feat: login endpoint partial done

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,19 @@ import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
 
 @Global()
 @Module({
+  imports: [TypeOrmModule.forFeature([Pessoa])],
   controllers: [AuthController],
   providers: [
     {
       provide: HashingService,
       useClass: BcryptService,
     },
-    AuthService
+    AuthService,
   ],
   exports: [HashingService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from "@nestjs/common";
-import { LoginDto } from "./dto/login.dto";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { Repository } from 'typeorm';
+import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HashingService } from './hashing/hashing.service';
 
 @Injectable()
 export class AuthService {
-    async login(loginDto: LoginDto) {
-        return loginDto;
+  constructor(
+    @InjectRepository(Pessoa)
+    private readonly pessoaRepository: Repository<Pessoa>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async login(loginDto: LoginDto) {
+    const pessoa = await this.pessoaRepository.findOneBy({
+      email: loginDto.email,
+    });
+
+    if (!pessoa) {
+      throw new UnauthorizedException('Pessoa não existe');
     }
+
+    const passwordIsValid = await this.hashingService.compare(
+      loginDto.password,
+      pessoa.passwordHash,
+    );
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Usuário ou senha inválidos');
+    }
+
+    return {
+        message: "Usuário logado!",
+    }
+  }
 }
